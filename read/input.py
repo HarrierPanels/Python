75827eb71f08937329bf56c97af98e0fdb8048ac
HarrierPanels <adsnse@gmail.com>
2022-09-09 00:23:25 +0300

    Read input

---
+++ read/input.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+name=input("Enter something ")
+if name=="hi":
+    print(name, "you")
+elif name == "by":
+    print(name, "\nyou")
+elif name == "What":
+    print(name, "you want")
+else:
+    print("good to go, %s!" % name)
+

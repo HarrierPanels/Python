958c41603f7c9c7d53e84423b1da522cdad6e010
HarrierPanels <108696009+HarrierPanels@users.noreply.github.com>
2022-09-16 14:34:38 +0300

    Multiple answers implemented

--- quiz.py
+++ quiz.py
@@ -43,24 +43,38 @@ def ask_question(question):
         print(f"The answer is {correct_answer!r}, not {answer!r}")
         return 0
 
-# Get answer    
-def get_answer(question, alternatives):
+# Get answers    
+def get_answers(question, alternatives, num_choices=1):
     print(f"{question}?")
     labeled_alternatives = dict(zip(ascii_lowercase, alternatives))
     for label, alternative in labeled_alternatives.items():
         print(f"  {label}) {alternative}")
-    # No Walrus    
+
     while True:
-    	answer_label = input(f"\nWhat's your choice? ")
-    	if answer_label in labeled_alternatives:
-        	break
-    	print(f"Please answer one of {', '.join(labeled_alternatives)}")
+        plural_s = "" if num_choices == 1 else f"s (choose {num_choices})"
+        answer = input(f"\nWhat's your choice{plural_s}? ")
+        answers = set(answer.replace(",", " ").split())
 
-# Python 3.8 Walrus       
-#    while (answer_label := input("\nChoice? ")) not in labeled_alternatives:
-#        print(f"Please answer one of {', '.join(labeled_alternatives)}")
+        # Handle invalid answers
+        if len(answers) != num_choices:
+            plural_s = "" if num_choices == 1 else "s, separated by comma"
+            print(f"Please answer {num_choices} alternative{plural_s}")
+            continue
 
-    return labeled_alternatives[answer_label]
+        if any(
+            # Python 3.8 Walrus # (invalid := answer) not in labeled_alternatives
+            invalid = answer
+    	    if invalid in labeled_alternatives:
+        	    break            
+            for answer in answers
+        ):
+            print(
+                f"{invalid!r} is not a valid choice. "
+                f"Please use {', '.join(labeled_alternatives)}"
+            )
+            continue
+
+        return [labeled_alternatives[answer] for answer in answers]
 
 # Driver call
 if __name__ == "__main__":
